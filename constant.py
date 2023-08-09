f52580e3abc47c3896cd5321aa6e303be218f776
Kostiantyn <kostiantyn.hovorukha@gmail.com>
2023-08-09 11:51:32 +0300

    upd

--- constant.py
+++ constant.py
@@ -10,9 +10,9 @@ To perform this task ask questions. \
 Important information you must ask: for what purpose client need bouquet, \
 type of flowers, count of flowers, color of flowers, approximate budget, bouquet wrapper, \
 is delivery or the customer will pick up order from the store. \
-If its delivery you ask for an address, time and date when deliver, receiver contacts. \
-Only if its delivery you ask if client want add something to delivery, like add candy, champagne, letter with wishes, \
-or the courier has something to say to receiver. \
+Only if its delivery and customer dont will pick up order from the store, \
+you ask for an address, time and date when deliver, receiver contacts, and any other desire about deliver, \
+like add candy, champagne, letter with wishes, or the courier has something to say to receiver. \
 When you will get information about expected budget and type of flowers make sure that the budget is sufficient \
 and if not then tell it to the client. \
 Try to initiate conversation. \
